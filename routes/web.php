73d08c813fb759f366b07b9a47ccc2f061a8d4ab
sanaatrsh <sanaatrash09@gmail.com>
2025-10-06 12:14:48 +0300

    test commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
